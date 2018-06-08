6a6499f5e18fb3e8320be0b45a30ca65a4470445
ahmbas <ahmed.bashir@dubizzle.com>
2018-06-08 16:03:25 -0400

    Update vragugent.sh

--- vragugent.sh
+++ vragugent.sh
@@ -1,7 +1,6 @@
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/root/bin
 export Playbook=$1
-yum -y install epel-release
-yum -y install ansible git
+apt-get install -y git ansible
 cd /tmp
 echo "localhost ansible_connection=local" >> /etc/ansible/hosts
 git clone https://github.com/vmtocloud/ansible-examples.git
